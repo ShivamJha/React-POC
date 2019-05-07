8b49d6e15cd495be4047087b8596b132a7ae5077
Shivam Kumar <shivamkr2801@gmail.com>
2019-05-07 16:27:52 +0530

    New Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import LoginScreen from './Loginscreen.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//import injectTapEventPlugin from 'react-tap-event-plugin';
+//Needed for onTouchTap
+//http://stackoverflow.com/a/34015469/988941
+//injectTapEventPlugin();
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      loginPage:[],
+	  dashboard:[]
+    }
+  }
+  componentWillMount(){
+    var loginPage =[];
+    loginPage.push(<LoginScreen appContext={this}/>);
+    this.setState({
+                  loginPage:loginPage
+                    })
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.loginPage}
+		{this.state.dashboard}
+      </div>
+    );
+  }
 }
 
 export default App;
